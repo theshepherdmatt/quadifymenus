c5aeab1993064897d628f03a4f19d05f15f4ba2c
Quadify <matt.theshepherd@gmail.com>
2024-06-11 12:58:34 +0100

    Update oledcontroller.js

--- oled/oledcontroller.js
+++ oled/oledcontroller.js
@@ -1,13 +1,12 @@
-const date = require('date-and-time');
-const oled = require('./oled.js');
-const fonts = require('./fonts.js');
+const APOled = require('./oled');
+const fonts = require('./fonts');
 
 const REFRESH_TRACK = 20;
 let api_state_waiting = false;
 let extn_exit_sleep_mode = false;
 let currentMode = 'clock';
 
-class APOled {
+class APOledController {
     constructor(opts, timeBeforeClock, timeBeforeScreensaver, timeBeforeDeepSleep) {
         this.opts = opts;
         this.TIME_BEFORE_CLOCK = timeBeforeClock;
@@ -35,9 +34,15 @@ class APOled {
         this.update_interval = null;
         this.refresh_track = REFRESH_TRACK;
         this.refresh_action = null;
-        this.driver = new oled(opts);
+        this.driver = new APOled(opts);
         this.dimmed = false;
+        this.pause_to_clock_timeout = null; // Add this line
     }
+
+    // Import methods for different modes
+    clock_mode = require('./modes/clock');
+    playback_mode = require('./modes/playback');
+    screensaver_mode = require('./modes/screensaver');
 
     volumio_seek_format(seek, duration) {
         let ratiobar = 0;
@@ -90,7 +95,16 @@ class APOled {
                 } else if (data.status !== "play" && currentMode !== "clock") {
                     currentMode = "clock";
                     if (this.page !== "clock") {
-                        this.clock_mode(); // Switch back to clock mode when playback stops
+                        // Delay before switching to clock mode
+                        if (!this.pause_to_clock_timeout) {
+                            this.pause_to_clock_timeout = setTimeout(() => {
+                                if (this.data.status !== "play") {
+                                    this.clock_mode(); // Switch to clock mode
+                                    currentMode = "clock";
+                                    this.pause_to_clock_timeout = null;
+                                }
+                            }, 60000); // Delay for 1 minute
+                        }
                     }
                 }
 
@@ -148,194 +162,10 @@ class APOled {
         }
     }
 
-    snake_screensaver() {
-        if (this.page === "snake_screensaver") return;
-        clearInterval(this.update_interval);
-        this.page = "snake_screensaver";
-
-        let box_pos = [0, 0];
-        let count = 0;
-        let flip = false;
-        let tail = [];
-        let tail_max = 25;
-        let t_tail_length = 1;
-        let random_pickups = [];
-        let screen_saver_animation_reset = () => {
-            tail = [];
-            count = 0;
-            t_tail_length = 10;
-            random_pickups = [];
-            let nb = 7;
-            while (nb--) {
-                let _x = Math.floor(Math.random() * (this.width));
-                let _y = Math.floor(Math.random() * (this.height / 3)) * 3;
-                random_pickups.push([_x, _y]);
-            }
-        }
-        screen_saver_animation_reset();
-        this.refresh_action = () => {
-            this.driver.buffer.fill(0x00);
-            let x;
-            if (count % this.width == 0) { flip = !flip }
-            if (flip) x = count % this.width + 1
-            else x = this.width - count % this.width
-            let y = ~~(count / this.width) * 3
-            tail.push([x, y]);
-            if (tail.length > t_tail_length) tail.shift();
-            for (let i of tail) {
-                this.driver.fillRect(i[0], i[1] - 1, 2, 3, 1);
-            }
-            for (let r of random_pickups) {
-                if (((flip && x >= r[0]) || (!flip && x <= r[0])) && y >= r[1]) {
-                    t_tail_length += 5;
-                    random_pickups.splice(random_pickups.indexOf(r), 1)
-                }
-                this.driver.fillRect(r[0], r[1], 1, 1, 1);
-            }
-            count++;
-            this.driver.update(true);
-            if (y > this.height) screen_saver_animation_reset();
-        }
-        this.update_interval = setInterval(() => { this.refresh_action() }, 40);
-    }
-
     deep_sleep() {
-        if (this.page === "deep_sleep") return;
-        this.status_off = true;
-        clearInterval(this.update_interval);
-        this.page = "deep_sleep";
+        console.log('Entering deep sleep mode...');
+        // Add logic to turn off the display or reduce its brightness significantly
         this.driver.turnOffDisplay();
-    }
-
-    clock_mode() {
-        if (this.page === "clock") return;
-        clearInterval(this.update_interval);
-        this.page = "clock";
-
-        this.refresh_action = () => {
-            this.driver.buffer.fill(0x00);
-            let ftime = date.format(new Date(), 'HH:mm');
-
-            this.driver.setCursor(70, 15);
-            this.driver.writeString(fonts.monospace, 4, ftime, 8);
-
-            this.driver.update(true);
-        }
-        this.refresh_action();
-        this.update_interval = setInterval(() => { this.refresh_action() }, 1000);
-    }
-
-    playback_mode() {
-        if (this.page === "playback") return;
-        clearInterval(this.update_interval);
-
-        this.scroller_x = 0;
-        this.page = "playback";
-        this.text_to_display = this.text_to_display || "";
-        this.refresh_track = REFRESH_TRACK;
-        this.refresh_action = () => {
-            if (this.plotting) { return }; // skip plotting of this frame if the pi has not finished plotting the previous frame
-            this.plotting = true;
-
-            this.driver.buffer.fill(0x00);
-
-            if (this.data) {
-                // volume
-                if (this.data.volume !== null) {
-                    let volstring = this.data.volume.toString();
-                    if (this.data.mute === true || volstring === "0") volstring = "X";
-
-                    this.driver.setCursor(4, this.height - 20); // Move volume display down
-                    this.driver.writeString(fonts.icons, 1, "0", 5); // Volume icon
-                    this.driver.setCursor(14, this.height - 19); // Adjust accordingly
-                    this.driver.writeString(fonts.monospace, 1, volstring, 5); // Volume level
-                }
-
-                // Repeat Single or Repeat All
-                if (this.data.repeatSingle) {
-                    this.driver.setCursor(232, this.height - 20); // Move repeat single symbol down
-                    this.driver.writeString(fonts.icons, 1, "5", 5); // Repeat single symbol
-                } else if (this.data.repeat) {
-                    this.driver.setCursor(232, this.height - 20); // Move repeat all symbol down
-                    this.driver.writeString(fonts.icons, 1, "4", 5); // Repeat all symbol
-                }
-
-                if (this.data) {
-                    // Combine trackType and footertext
-                    let combinedInfo = `${this.data.trackType || ''} ${this.footertext || ''}`.trim();
-
-                    // Assuming an average character width for calculation
-                    let combinedInfoWidth = combinedInfo.length * 6; // Adjust the multiplier based on your font and display
-                    let startX = (this.width - combinedInfoWidth) / 2; // Calculate X to center the combined string
-                    let infoYPosition = this.height - 20; // Adjust Y position as needed
-
-                    this.driver.setCursor(startX, infoYPosition);
-                    this.driver.writeString(fonts.monospace, 1, combinedInfo, 5);
-                }
-
-                // play pause stop logo
-                if (this.data.status) {
-                    let status_symbol = "";
-                    switch (this.data.status) {
-                        case ("play"):
-                            status_symbol = "1";
-                            break;
-                        case ("pause"):
-                            status_symbol = "2"
-                            break;
-                        case ("stop"):
-                            status_symbol = "3"
-                            break;
-                    }
-
-                    this.driver.setCursor(246, this.height - 20); // Move play/pause/stop logo down
-                    this.driver.writeString(fonts.icons, 1, status_symbol, 6);
-                }
-
-                if (this.text_to_display.length) {
-                    let splitIndex = this.text_to_display.indexOf(" - ");
-                    let title = this.text_to_display.substring(0, splitIndex);
-                    let artist = this.text_to_display.substring(splitIndex + 3);
-
-                    const handleTextDisplay = (text, initialY) => {
-                        let textWidth = this.driver.getStringWidthUnifont(text);
-                        if (textWidth > this.width) {
-                            if (!this.scrollX) this.scrollX = 0;
-                            this.driver.cursor_x = this.scrollX;
-                            this.scrollX = this.scrollX - 1 < -textWidth ? this.width : this.scrollX - 1;
-                        } else {
-                            this.driver.cursor_x = (this.width - textWidth) / 2;
-                        }
-                        this.driver.cursor_y = initialY;
-                        this.driver.writeStringUnifont(text, 6);
-                    };
-
-                    handleTextDisplay(title, 0); // For title
-                    handleTextDisplay(artist, 18); // For artist, placed below the title
-                }
-
-                if (this.data.seek_string) {
-                    let border_right = this.width - 5;
-                    let bottomY = this.height - 7;
-                    this.driver.drawLine(3, bottomY, border_right, bottomY, 3);
-                    this.driver.drawLine(border_right, bottomY, border_right, this.height - 4, 3);
-                    this.driver.drawLine(3, this.height - 4, border_right, this.height - 4, 3);
-                    this.driver.drawLine(3, this.height - 4, 3, bottomY, 3);
-                    this.driver.fillRect(3, bottomY, this.data.ratiobar, 4, 4);
-                    this.driver.cursor_y = 43;
-                    this.driver.cursor_x = 93;
-                    this.driver.writeString(fonts.monospace, 0, this.data.seek_string, 5);
-                }
-            }
-
-            this.driver.update();
-            this.plotting = false;
-            if (this.refresh_track) return this.refresh_track--;
-            this.scroller_x--;
-        }
-
-        this.update_interval = setInterval(() => { this.refresh_action() }, this.opts.main_rate);
-        this.refresh_action();
     }
 
     handle_sleep(exit_sleep) {
@@ -343,7 +173,7 @@ class APOled {
             if (!this.iddle_timeout) { // Check if the screen is not already waiting to go into sleep mode (instruction initiated in a previous cycle).
                 let _deepsleep_ = () => { this.deep_sleep(); }
                 let _screensaver_ = () => {
-                    this.snake_screensaver();
+                    this.screensaver_mode();
                     this.iddle_timeout = setTimeout(_deepsleep_, this.TIME_BEFORE_DEEPSLEEP);
                 }
                 let _clock_ = () => {
@@ -368,8 +198,27 @@ class APOled {
                 clearTimeout(this.iddle_timeout);
                 this.iddle_timeout = null;
             }
+
+            if (this.data.status === "pause") {
+                // Set a timeout to switch to clock mode after a minute if still paused
+                if (!this.pause_to_clock_timeout) {
+                    this.pause_to_clock_timeout = setTimeout(() => {
+                        if (this.data.status === "pause") {
+                            this.clock_mode();
+                            currentMode = "clock";
+                        }
+                        this.pause_to_clock_timeout = null;
+                    }, 60000); // 1 minute
+                }
+            } else if (this.data.status === "play" || this.data.status === "stop") {
+                // Clear the pause to clock timeout if the status changes
+                if (this.pause_to_clock_timeout) {
+                    clearTimeout(this.pause_to_clock_timeout);
+                    this.pause_to_clock_timeout = null;
+                }
+            }
         }
     }
 }
 
-module.exports = APOled;
+module.exports = APOledController;
